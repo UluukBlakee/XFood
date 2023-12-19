db30024702202f91f1582421f09cc4c58aa64978
maratovaruuke <myrzalievaruuke1@gmail.com>
2023-12-20 02:25:16 +0600

    added endpoint for pizzeria  create

--- src/XFood.API/Check-List/Queries/CheckListView.cs
+++ src/XFood.API/Check-List/Queries/CheckListView.cs
@@ -5,7 +5,7 @@ namespace XFood.API.Check_List.Queries
     public class CheckListView
     {
         public int Id { get; set; }
-        public Pizzeria? Pizzeria{ get; set; }
+        public XFood.Data.Models.Pizzeria? Pizzeria{ get; set; }
         public int TotalPoints { get; set; }
         public List<Criterion>? Criteria { get; set; }
         public List<CategoryFactor>? CategoryFactors { get; set; }
