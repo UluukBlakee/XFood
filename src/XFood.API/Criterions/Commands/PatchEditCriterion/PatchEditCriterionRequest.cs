5a633b43f2feb17c19c776d3eb154d0560538248
Uluk <talantbuluk@gmail.com>
2024-01-21 23:25:27 +0600

    Added Update, Delete Criterion

--- src/XFood.API/Criterions/Commands/PatchEditCriterion/PatchEditCriterionRequest.cs
+++ src/XFood.API/Criterions/Commands/PatchEditCriterion/PatchEditCriterionRequest.cs
@@ -5,7 +5,6 @@ namespace XFood.API.Criterions.Commands.PatchEditCriterion
 {
     public class PatchEditCriterionRequest
     {
-        [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public int MaxPoints { get; set; }
---
+++ src/XFood.Web/Services/Criterion/Create/CreateCriterionRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XFoodBlazor.Web.Client.Services.Criterion.Create
+{
+    public class CreateCriterionRequest 
+    {
+        public string Name { get; set; }
+        [Required] public int MaxPoints { get; set; }
+        public string Section { get; set; }
+        [Required] public int ReceivedPoints { get; set; }
+        [Required] public int PizzeriaId { get; set; }   
+    }
+}
