259b2ac0e434b648bfddb4ff1b242375f775156d
maratovaruuke <myrzalievaruuke1@gmail.com>
2024-01-28 23:42:34 +0600

    added create pizzeria for admin

--- src/XFood.Web/Services/Pizzeria/PizzeriaService.cs
+++ src/XFood.Web/Services/Pizzeria/PizzeriaService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using XFoodBlazor.Web.Client.Services.Pizzeria.Create;
 using XFoodBlazor.Web.Client.Services.Pizzeria.GetList;
 using XFoodBlazor.Web.Client.Services.Pizzeria.GetPizzeria;
 
@@ -8,6 +9,7 @@ namespace XFoodBlazor.Web.Client.Services.Pizzeria
     {
         Task<GetListPizzeriaResponse> GetListPizzeria(GetListPizzeriaRequest pizzeriaModel);
         Task<GetPizzeriaResponse> GetPizzeria(GetPizzeriaRequest pizzeriaModel);
+        Task<CreatePizzeriaResponse> CreatePizzeria(CreatePizzeriaRequest pizzeriaModel);
     }
 
     public class PizzeriaService : IPizzeriaService
@@ -39,6 +41,19 @@ namespace XFoodBlazor.Web.Client.Services.Pizzeria
                 return response;
             }
             return new GetPizzeriaResponse(null);
+        }
+
+        public async Task<CreatePizzeriaResponse> CreatePizzeria(CreatePizzeriaRequest pizzeriaModel)
+        {
+            var result = await _httpClient.PostAsJsonAsync("pizzeria", pizzeriaModel);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var response = await result.Content.ReadFromJsonAsync<CreatePizzeriaResponse>();
+                return response;
+            }
+
+            return new CreatePizzeriaResponse(false, await result.Content.ReadAsStringAsync());
         }
     }
 }
