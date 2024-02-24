5d3dfb6578fba2fecb0b799fda1df6e7ede689af
Malika <emalika654@gmail.com>
2024-02-24 22:05:39 +0600

    Implemented the addition of managers in the client part

---
+++ src/XFood.Web/Services/Manager/Create/CreateManagerResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace XFoodBlazor.Web.Client.Services.Manager.Create
+{
+    public record CreateManagerResponse(bool IsSuccess);
+}
--- src/XFood.Web/Services/Manager/ManagerService.cs
+++ src/XFood.Web/Services/Manager/ManagerService.cs
@@ -1,13 +1,15 @@
 ﻿using System.Net.Http.Json;
 using XFoodBlazor.Web.Client.Services.Manager.Delete;
 using XFoodBlazor.Web.Client.Services.Manager.GetList;
+using XFoodBlazor.Web.Client.Services.Manager.Create;
 
 namespace XFoodBlazor.Web.Client.Services.Manager
 {
     public interface IManagerService
     {
         Task<GetManagersResponse> GetManagers(GetManagersRequest managerModel);
-        Task<DeleteManagersResponse> Delete(DeleteManagersRequest criticalFactorModel);
+        Task<DeleteManagersResponse> Delete(DeleteManagersRequest managerModel);
+        Task<CreateManagerResponse> Create(CreateManagerRequest managerModel);
     }
     public class ManagerService : IManagerService
     {
@@ -26,15 +28,25 @@ namespace XFoodBlazor.Web.Client.Services.Manager
             }
             return new GetManagersResponse(null);
         }
-        public async Task<DeleteManagersResponse> Delete(DeleteManagersRequest criticalFactorModel)
+        public async Task<DeleteManagersResponse> Delete(DeleteManagersRequest managerModel)
         {
-            var result = await _httpClient.DeleteAsync($"manager/{criticalFactorModel.Id}");
+            var result = await _httpClient.DeleteAsync($"manager/{managerModel.Id}");
             if (result.IsSuccessStatusCode)
             {
                 var response = await result.Content.ReadFromJsonAsync<DeleteManagersResponse>();
                 return response;
             }
             return new DeleteManagersResponse(false);
+        }
+        public async Task<CreateManagerResponse> Create(CreateManagerRequest managerModel)
+        {
+            var result = await _httpClient.PostAsJsonAsync("manager", managerModel);
+            if (result.IsSuccessStatusCode)
+            {
+                var response = await result.Content.ReadFromJsonAsync<CreateManagerResponse>();
+                return response;
+            }
+            return new CreateManagerResponse(false);
         }
     }
 }
