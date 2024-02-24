3aebcf620fcd44652f45d9b3ae9a4ec98aaf58dc
Malika <emalika654@gmail.com>
2024-02-24 17:58:21 +0600

    Manager page has been created, where a list of all managers is displayed

--- src/XFood.Web/Services/Manager/ManagerView.cs
+++ src/XFood.Web/Services/Manager/ManagerView.cs
@@ -7,6 +7,7 @@ namespace XFoodBlazor.Web.Client.Services.Manager
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? Email { get; set; }
         public PizzeriaView? Pizzeria { get; set; }
     }
 }
